5de837654463a9a0de61186665a4ce682ee9daba
Jon Skinner <jon.a.skinner1@gmail.com>
2022-01-12 15:48:33 -0500

    adding babel and gulp configs

--- es6/scripts.js
+++ es6/scripts.js
@@ -1,9 +1,11 @@
-var randomize = function (min, max) {
+'use strict'
+
+var randomize = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
   },
-  getNumber = function (arr, draw) {
+  getNumber = (arr, draw) => {
     var getNewNum = randomize(1, 43);
     if (!draw) {
       if (checkTens(arr, getNewNum)) {
@@ -16,23 +18,23 @@ var randomize = function (min, max) {
     }
     return getNewNum;
   },
-  checkDuplicates = function (arr, elm) {
+  checkDuplicates = (arr, elm) => {
     return arr.indexOf(elm) == -1;
   },
-  checkTens = function (arr, elm) {
+  checkTens = (arr, elm) => {
     return arr.indexOf(elm - 10) != -1 || arr.indexOf(elm + 10) != -1;
   },
-  checkAdj = function (arr, elm) {
+  checkAdj = (arr, elm) => {
     return arr.indexOf(elm - 1) != -1 || arr.indexOf(elm + 1) != -1;
   },
-  outOfBounds = function (arr, elm) {
+  outOfBounds = (arr, elm) => {
     arr.reduce(sumThisUp, 0) + elm < 86 || arr.reduce(sumThisUp, 0) + elm > 129;
   },
-  sumThisUp = function (accumulator, a) {
+  sumThisUp = (accumulator, a) => {
     return accumulator + a;
   },
-  createOneSet = function (draw) {
-    arr = [];
+  createOneSet = draw => {
+    let arr = [];
 
     while (arr.length < 5) {
       var newNumber = getNumber(arr, draw);
@@ -46,13 +48,13 @@ var randomize = function (min, max) {
       return a - b;
     });
   },
-  containsAll = function (needles, haystack) {
+  containsAll = (needles, haystack) => {
     for (var i = 0; i < needles.length; i++) {
       if (haystack.indexOf(needles[i]) == -1) return false;
     }
     return true;
   },
-  getMyPicks = function (tix) {
+  getMyPicks = tix => {
     var myPicks = [];
     while (myPicks.length < tix) {
       var newArr = createOneSet(false);
@@ -62,7 +64,7 @@ var randomize = function (min, max) {
     }
     return myPicks;
   },
-  checkPicksStats = function (pix) {
+  checkPicksStats = pix => {
     var days = 0;
     var draw = 0;
     var hit = false;
