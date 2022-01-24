7a529dd743affe1a7a74eda8758655e69754eda1
Jon Skinner <jon.a.skinner1@gmail.com>
2022-01-24 18:00:36 -0500

    added logging; updated routing;

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
 const path = require("path");
 const lotto = require("./handlers/lotto");
 
+const app = express().use(morgan("dev"));
+
 app.use(express.static(__dirname + "/../public"));
 
-app.get("/", lotto.handle_pick_request);
+app.get("/", (req, res) => res.end("home"));
 
-app.get("/picks/:tix", lotto.handle_pick_request);
+app.get("/pick", lotto.handle_pick_request);
 
 // generic route not found
 // TODO: replace with better function
-// app.get("*", (req, res) => res.end("404: Route not found"));
+app.get("*", (req, res) => res.end("404: Route not found"));
 
 app.listen(3000);
